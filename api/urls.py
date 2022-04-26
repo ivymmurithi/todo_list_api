33e2cb5150f1e8cc881a19e04de82ea888ef0c07
ivymmurithi <ivymurithi@gmail.com>
2022-04-26 13:44:19 +0300

    authentication

---
+++ api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from api.views import UserViewSet
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='api-auth'))
+]
